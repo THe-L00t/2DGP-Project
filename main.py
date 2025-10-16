5547f3c2785136c156fa5101a264d82801916898
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-10-17 00:15:13 +0900

    main 기본 틀 구성

---
+++ main.py
@@ -0,0 +1,41 @@
+from pico2d import *
+
+#----------------------------------------------------------------
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            pass
+
+#----------------------------------------------------------------
+def init_world():
+    global world
+    world = []
+#----------------------------------------------------------------
+def update_world():
+    for object in world:
+        object.update()
+#----------------------------------------------------------------
+def render_world():
+    clear_canvas()
+    for object in world:
+        object.draw()
+    update_canvas()
+#----------------------------------------------------------------
+running = True
+open_canvas()
+init_world()
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    #추후 deltaTime 적용해보기
+
+close_canvas()
